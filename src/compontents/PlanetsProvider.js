497680b208c99b45e3b93b5498e7e6141b654f7e
Igor Sampaio <igorsamf@gmail.com>
2022-08-24 19:09:49 -0300

    requisito 1

--- src/compontents/PlanetsProvider.js
+++ src/compontents/PlanetsProvider.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
 import MyContex from './MyContext';
 
@@ -26,4 +27,9 @@ function PlanetsProvider({ children }) {
     </MyContex.Provider>
   );
 }
+
+PlanetsProvider.propTypes = {
+  children: PropTypes.arrayOf(Object).isRequired,
+};
+
 export default PlanetsProvider;
